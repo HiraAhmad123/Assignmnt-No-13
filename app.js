ec2c0e7a55ef745997014451133b77df79673930
HiraAhmad123 <hiraa1622@gmail.com>
2024-09-26 11:52:29 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,99 @@
+// Chapter # 01 (ALERTS)
+// Q no 1 -- 
+// i
+
+alert("First Name");
+
+// ii
+
+alert("Last Name");
+
+// iii
+
+alert("Email");
+
+// iv
+
+alert("Phone Number");
+
+// v
+
+alert("Password");
+
+// Q no 02
+
+alert("You are learning JavaScript!");
+
+// Q no 03
+
+alert("Welcome Users");
+
+// Chapter # 02 (Variables for string)
+
+// Q no 1
+
+var fullName = "hira";
+console.log(fullName);
+
+// Q no 2
+
+var userName;
+userName = "Ibrahim";
+console.log(userName); 
+
+// Q no 3
+
+var teamName ="Developers Team";
+alert(teamName);
+
+// Q no 4
+
+bestMan ="My Father";
+console.log(bestMan);
+
+// Chapter # no : 03 (Variables for numbers)
+
+// Q no 1
+
+var caseQty;
+console.log(caseQty);
+
+// Q no 2 
+
+caseQty = 144 ;
+console.log(caseQty)
+
+// Q no 3 
+
+var num="9";
+var changeinNumber = 9 ;
+console.log(changeinNumber);
+
+// Q no 4
+
+var addition;
+addition = 20 + 30;
+console.log(addition);
+
+// Q no 5
+
+var merchTotal = 100;
+var shippingCharge = 10;
+console.log(merchTotal + shippingCharge);
+
+// Q no 6
+
+var number = 60;
+number = 60 + 40;
+console.log(number);
+
+
+
+
+
+
+
+
+
+
+
